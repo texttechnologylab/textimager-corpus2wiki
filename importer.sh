da2984cc5233940e315a9706954c7f7f2a06ba3f
emarutherford <40407963+emarutherford@users.noreply.github.com>
2018-06-24 15:50:12 +0200

    Update importer.sh

--- importer.sh
+++ importer.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-sed -i 's/<span class="sentence">//g' /home/eleanor/textimager-client/target/output/output.wiki.xml
-sed -i 's/<\/span>//g' /home/eleanor/textimager-client/target/output/output.wiki.xml
+sed -i 's/<span class="sentence">//g' ~/textimager-client/target/output/output.wiki.xml
+sed -i 's/<\/span>//g' ~/textimager-client/target/output/output.wiki.xml
 
-docker cp /home/eleanor/textimager-client/target/output/output.wiki.xml wikiexporter_mediawiki_1:/var/www/html/maintenance/output.wiki.xml
+javac Counter.java
 
-docker cp /home/eleanor/SimpleTooltip-master/. wikiexporter_mediawiki_1:/var/www/html/extensions/SimpleTooltip-master
+wait
 
-docker exec wikiexporter_mediawiki_1 /bin/sh -c "cd maintenance;php importDump.php < output.wiki.xml;php rebuildrecentchanges.php;cd ..;echo 'require_once \"\$IP/extensions/SimpleTooltip-master/SimpleTooltip.php\";' >> LocalSettings.php"
+java Counter
 
+wait
 
+docker cp ~/textimager-client/target/output/output.wiki.xml wikiexporter_mediawiki_1:/var/www/html/maintenance/output.wiki.xml
 
+docker cp ~/SimpleTooltip-master/. wikiexporter_mediawiki_1:/var/www/html/extensions/SimpleTooltip-master
 
+docker cp ~/WikiExporter/JsonConfig/. wikiexporter_mediawiki_1:/var/www/html/extensions/JsonConfig
+
+docker cp ~/WikiExporter/Graph/. wikiexporter_mediawiki_1:/var/www/html/extensions/Graph
+
+docker exec wikiexporter_mediawiki_1 /bin/sh -c "cd maintenance;php importDump.php < output.wiki.xml;php rebuildrecentchanges.php;cd ..;echo 'require_once \"\$IP/extensions/SimpleTooltip-master/SimpleTooltip.php\";' >> LocalSettings.php; echo 'wfLoadExtension( 'JsonConfig' );' >> LocalSettings.php; echo 'wfLoadExtension( 'Graph' );' >> LocalSettings.php"
 
 
