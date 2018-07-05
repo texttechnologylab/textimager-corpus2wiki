aefa8e3e42b4728460dd216a7f73625521b3d465
emarutherford <40407963+emarutherford@users.noreply.github.com>
2018-07-05 19:04:45 +0200

    Update importer.sh

--- importer.sh
+++ importer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sed -i 's/<span class="sentence">//g' ~/textimager-client/target/output/output.wiki.xml
-sed -i 's/<\/span>//g' ~/textimager-client/target/output/output.wiki.xml
+sed -i 's/<span class="sentence">//g' maintenance/output.wiki.xml
+sed -i 's/<\/span>//g' maintenance/output.wiki.xml
 
 javac Counter.java
 
@@ -11,14 +11,14 @@ java Counter
 
 wait
 
-docker cp ~/textimager-client/target/output/output.wiki.xml wikiexporter_mediawiki_1:/var/www/html/maintenance/output.wiki.xml
+docker cp ~/WikiExporter/maintenance/output.wiki.xml wikiexporter_mediawiki_1:/var/www/html/maintenance/output.wiki.xml
 
-docker cp ~/SimpleTooltip-master/. wikiexporter_mediawiki_1:/var/www/html/extensions/SimpleTooltip-master
+docker cp ~/WikiExporter/SimpleTooltip-master/. wikiexporter_mediawiki_1:/var/www/html/extensions/SimpleTooltip-master
 
 docker cp ~/WikiExporter/JsonConfig/. wikiexporter_mediawiki_1:/var/www/html/extensions/JsonConfig
 
 docker cp ~/WikiExporter/Graph/. wikiexporter_mediawiki_1:/var/www/html/extensions/Graph
 
-docker exec wikiexporter_mediawiki_1 /bin/sh -c "cd maintenance;php importDump.php < output.wiki.xml;php rebuildrecentchanges.php;cd ..;echo 'require_once \"\$IP/extensions/SimpleTooltip-master/SimpleTooltip.php\";' >> LocalSettings.php; echo 'wfLoadExtension( 'JsonConfig' );' >> LocalSettings.php; echo 'wfLoadExtension( 'Graph' );' >> LocalSettings.php"
+docker exec wikiexporter_mediawiki_1 /bin/sh -c "cd maintenance;php importDump.php < output.wiki.xml;php rebuildrecentchanges.php;cd ..;echo 'require_once \"\$IP/extensions/SimpleTooltip/SimpleTooltip.php\";' >> LocalSettings.php; echo 'wfLoadExtension( '\''JsonConfig'\'' );' >> LocalSettings.php; echo 'wfLoadExtension( '\''Graph'\'' );' >> LocalSettings.php"
 
 
