6788622b06710caef4959339c0cb783e567d182b
emarutherford <40407963+emarutherford@users.noreply.github.com>
2018-07-05 19:07:13 +0200

    Update dumper.sh

--- dumper.sh
+++ dumper.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-java -jar textimager-CLI.jar -i '../src/test/resources/collectionTest2' --input-format TXT --input-language en -output output --output-format MEDIAWIKI -p "LanguageToolSegmenter,LanguageToolLemmatizer,StanfordPosTagger"
-
-
-
-
+java -jar textimager-CLI.jar -i 'corpus' --input-format TXT --input-language en -output maintenance --output-format MEDIAWIKI -p "LanguageToolSegmenter,LanguageToolLemmatizer,StanfordPosTagger"
