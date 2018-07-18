d0c724177c8c1825a7808813161d6fc75c372562
Wahed Hem <w.hemati@gmail.com>
2018-07-18 12:26:27 +0200

    cleanup importer

---
+++ corpusImporter.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+ 
+java -jar textimager-CLI.jar -i $1 --input-format TXT --input-language $2 -output maintenance --output-format MEDIAWIKI -p "LanguageToolSegmenter,LanguageToolLemmatizer,StanfordPosTagger" 
+ 
+
