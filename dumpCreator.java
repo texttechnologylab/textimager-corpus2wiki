df811f8a0ef7807959a65a9fa2e6eeb7897c9704
Alex Hunziker <alexhunziker@sunrise.ch>
2018-10-30 17:17:56 +0100

    Fixed portability issue

--- dumpCreator.java
+++ dumpCreator.java
@@ -4,17 +4,21 @@ import java.io.*;
 public class dumpCreator{
     
     public static void main (String[] args) throws IOException,InterruptedException{
+	System.out.println("Working Directory =  " + System.getProperty("user.dir"));
 
 	//textimager preprocessing - create the dump
-	ProcessBuilder buildDump = new ProcessBuilder("/home/eleanor/WikiExporter/dumper.sh");
-	buildDump.directory(new File("/home/eleanor/textimager-client/target"));
+	ProcessBuilder buildDump = new ProcessBuilder("./dumper.sh");
+	buildDump.directory(new File(System.getProperty("user.dir")));
 	Process textimager = buildDump.start();
 	textimager.waitFor();
+	System.out.println("Dump created");
 
 	//calls the import.sh script - preprocessing of dump + importation of dump, extensions 	
-	ProcessBuilder buildImport = new ProcessBuilder("/home/eleanor/WikiExporter/importer.sh");
-	buildImport.directory(new File("/home/eleanor/WikiExporter"));
+	ProcessBuilder buildImport = new ProcessBuilder("./importer.sh");
+	buildImport.directory(new File(System.getProperty("user.dir")));
 	Process importer = buildImport.start();
+	importer.waitFor();
+	System.out.println("Importer finished");
     }
     
 }
