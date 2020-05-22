e4de982a74ba4f33c54defba237afc91aad0bd3d
Wahed Hemati <hemati@em.uni-frankfurt.de>
2020-05-22 13:12:23 +0200

    Update importer.php

--- corpus2wiki/import/importer.php
+++ corpus2wiki/import/importer.php
@@ -15,8 +15,8 @@ if($lang == ""){
 	$default_lan = 1;
 }
 $DEFAULT_PIPELINES = [
-	"en" => "LanguageToolSegmenter,LanguageToolLemmatizer,StanfordPosTagger,StanfordNamedEntityRecognizer,FastTextDDCMulLemmaNoPunctPOSNoFunctionwordsWithCategoriesService,MateMorphTagger",
-	"de" => "LanguageToolSegmenter,LanguageToolLemmatizer,StanfordPosTagger,StanfordNamedEntityRecognizer,FastTextDDCMulLemmaNoPunctPOSNoFunctionwordsWithCategoriesService,TagMeLocalAnnotator,MateMorphTagger",
+	"en" => "LanguageToolSegmenter,LanguageToolLemmatizer,CoreNlpPosTagger,CoreNlpNamedEntityRecognizer,FastTextDDCMulLemmaNoPunctPOSNoFunctionwordsWithCategoriesService,MateMorphTagger",
+	"de" => "LanguageToolSegmenter,LanguageToolLemmatizer,CoreNlpPosTagger,CoreNlpNamedEntityRecognizer,FastTextDDCMulLemmaNoPunctPOSNoFunctionwordsWithCategoriesService,TagMeLocalAnnotator,MateMorphTagger",
 ];
 if(array_key_exists($lang, $DEFAULT_PIPELINES)){
 	$pipeline = $DEFAULT_PIPELINES[$lang];
