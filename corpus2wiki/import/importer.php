9e2d4d78998ee297e734ac8783f80b567d085b35
Maschamagin <35238192+Maschamagin@users.noreply.github.com>
2021-05-19 10:38:31 +0200

    Update importer.php

--- corpus2wiki/import/importer.php
+++ corpus2wiki/import/importer.php
@@ -9,13 +9,13 @@ error_reporting(E_ALL);
 // Get parameters
 $default_lan = 0;
 $lang = $_POST["language"];
-$corpus_dir = "import/corpus";
+$corpus_dir = "/var/www/html/import/corpus";
 if($lang == ""){
 	$lang = "en";
 	$default_lan = 1;
 }
 $DEFAULT_PIPELINES = [
-	"en" => "LanguageToolSegmenter,LanguageToolLemmatizer,CoreNlpPosTagger,SpaCyNER,FastTextDDCMulLemmaNoPunctPOSNoFunctionwordsWithCategoriesTextImagerService,TagMeAPIAnnotator",
+	"en" => "LanguageToolLemmatizer,CoreNlpPosTagger,SpaCyNER,FastTextDDCMulLemmaNoPunctPOSNoFunctionwordsWithCategoriesTextImagerService,TagMeAPIAnnotator",
 	"de" => "LanguageToolSegmenter,LanguageToolLemmatizer,CoreNlpPosTagger,SpaCyNER,FastTextDDCMulLemmaNoPunctPOSNoFunctionwordsWithCategoriesTextImagerService,TagMeLocalAnnotator,MateMorphTagger",
 ];
 if(array_key_exists($lang, $DEFAULT_PIPELINES)){
@@ -94,11 +94,11 @@ echo "Upload data to Corpus2Wiki ($countfiles files)...";
 for($i=0;$i<$countfiles;$i++){
 	$filename = $_FILES['file']['name'][$i];
 	// Upload file
-	move_uploaded_file($_FILES['file']['tmp_name'][$i],'corpus/'.$filename);
+	move_uploaded_file($_FILES['file']['tmp_name'][$i],'/var/www/html/import/corpus/'.$filename);
 	if(strpos($filename, ".zip")){
 		echo "Extracting ".$filename."...";
 		exec("cd /var/www/html/import/corpus; unzip -n -j ".$filename.";cd ..", $log1);
-		unlink("corpus/".$filename);
+		unlink("/var/www/html/import/corpus/".$filename);
 	}
 }
 echo "Files to process: ".(count(scandir("corpus")) - 2)."...";
@@ -114,7 +114,7 @@ $backup_file = $corpus_dir . "/backup.xml";
 if(file_exists($backup_file)) {
 	unlink($backup_file);
 }
-exec("php maintenance/dumpBackup.php --full > ".$backup_file, $log1);
+exec("php /var/www/html/maintenance/dumpBackup.php --full > ".$backup_file, $log1);
 if(file_exists($backup_file)) {
 	echo "<b>done</b><br>";
 } else {
@@ -125,11 +125,11 @@ echo '<script>set_progress(7);</script>';
 
 // Step 2: Call Textimager
 echo "Analyze Texts...";
-if(file_exists("maintainance/output.wiki.xml")){
-	unlink("maintainance/output.wiki.xml");
+if(file_exists("/var/www/html/import/maintenance/output.wiki.xml")){
+	unlink("/var/www/html/import/maintenance/output.wiki.xml");
 }
 putenv("SHELL=/bin/bash");
-liveExecuteCommand("nohup java -Xms512m -Xmx4g -jar textimager-CLI.jar -i 'corpus' --input-format TXT --input-language ".$lang." -output maintenance --output-format MEDIAWIKI -p '$pipeline'");
+liveExecuteCommand("nohup java -Xms512m -Xmx4g -jar /var/www/html/import/textimager-CLI.jar -i '/var/www/html/import/corpus' --input-format TXT --input-language ".$lang." -output /var/www/html/import/maintenance --output-format MEDIAWIKI -p '$pipeline'");
 if(file_exists("maintenance/output.wiki.xml")){
 	echo "<b>done</b><br>";
 } else {
@@ -140,30 +140,28 @@ echo '<script>set_progress(60);</script>';
 
 // Step 3: Prepare for import
 echo "Prepare texts for Corpus2Wiki import...";
-exec("sed -i 's/Ä/\&#196;/g;s/Ö/\&#214;/g;s/Ü/\&#220;/g;s/ä/\&#228;/g;s/ö/\&#246;/g;s/ü/\&#252;/g;s/ß/\&#223;/g' maintenance/output.wiki.xml", $log3);
+exec("sed -i 's/Ä/\&#196;/g;s/Ö/\&#214;/g;s/Ü/\&#220;/g;s/ä/\&#228;/g;s/ö/\&#246;/g;s/ü/\&#252;/g;s/ß/\&#223;/g' /var/www/html/import/maintenance/output.wiki.xml", $log3);
 echo "<b>done</b><br>";
 //liveExecuteCommand("javac Counter.java; wait; java Counter;wait");
 echo '<script>set_progress(80);</script>';
 
 // Step 4: Import into Mediawiki
 echo "Import texts into Corpus2Wiki...";
-liveExecuteCommand("php ../maintenance/importDump.php < maintenance/output.wiki.xml");
-liveExecuteCommand("php ../maintenance/rebuildrecentchanges.php");
+liveExecuteCommand("php /var/www/html/maintenance/importDump.php < maintenance/output.wiki.xml");
+liveExecuteCommand("php /var/www/html/maintenance/rebuildrecentchanges.php");
 echo '<b>done</b><br>';
 echo '<script>set_progress(95);</script>';
 
 // Step 5: Clean up
 echo "Cleaning up...";
-if(file_exists("maintainance/output.wiki.xml")){
-	unlink("maintainance/output.wiki.xml");
+if(file_exists("/var/www/html/import/maintenance/output.wiki.xml")){
+	unlink("/var/www/html/import/maintenance/output.wiki.xml");
 }
-array_map('unlink', array_filter((array) glob("corpus/*")));
+array_map('unlink', array_filter((array) glob("/var/www/html/import/corpus/*")));
 echo '<b>done</b><br>';
 echo '<script>set_progress(100);</script>';
 
 // All done.
 echo '<p style="color:green;"><b>Input procedure terminated</b></p>';
 echo '<p>All documents that were processed sucessfully are now available in the <a href="../index.php/Special:AllPages"><b>Corpus2Wiki</b></a></p>';
-
-
 ?>
