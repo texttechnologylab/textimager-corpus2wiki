24446cdac0a7dc4255d5c2b220bcba19e5ab205d
Marcel Faizi <49288103+MarcelFaizi@users.noreply.github.com>
2021-07-03 20:01:57 +0200

    Workaround for morphological features

--- utils/MediawikiWriterTest/src/test/java/Test.java
+++ utils/MediawikiWriterTest/src/test/java/Test.java
@@ -1,45 +1,47 @@
 import de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData;
-import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
-
+import de.tudarmstadt.ukp.dkpro.core.matetools.MateMorphTagger;
 import org.apache.commons.io.FileUtils;
 import org.apache.uima.UIMAException;
+import org.apache.uima.analysis_engine.AnalysisEngine;
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.cas.impl.XmiCasDeserializer;
-import org.apache.uima.cas.impl.XmiCasSerializer;
+import org.apache.uima.fit.factory.AggregateBuilder;
 import org.apache.uima.fit.factory.JCasFactory;
-import org.apache.uima.fit.pipeline.SimplePipeline;
-import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.fit.util.LifeCycleUtil;
 import org.apache.uima.jcas.JCas;
 import org.dkpro.core.io.xmi.XmiWriter;
 import org.hucompute.textimager.client.TextImagerClient;
 import org.hucompute.textimager.uima.io.mediawiki.MediawikiWriter;
-import de.tudarmstadt.ukp.dkpro.core.api.ner.type.NamedEntity;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.nio.charset.StandardCharsets;
-
-import javax.xml.transform.OutputKeys;
-
-import org.apache.uima.util.XMLInputSource;
-import org.apache.uima.util.XMLSerializer;
 
 import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+import static org.hucompute.textimager.uima.io.mediawiki.MediawikiWriter.PARAM_TARGET_LOCATION;
 
+class TTLabSimplePipeline {
+
+    public static void runPipeline(final JCas jCas, final AnalysisEngine... engines) throws UIMAException, IOException {
+
+        for (AnalysisEngine engine : engines) {
+            engine.process(jCas);
+            LifeCycleUtil.destroy(engine);
+
+        }
+    }
+
+}
 
 public class Test {
 @org.junit.Test
     public void WikiditionTest() throws IOException, UIMAException {
 
-        String sTools = "LanguageToolLemmatizer,SpaCyMultiTagger,TagMeAPIAnnotator";
+        String sTools = "LanguageToolSegmenter,LanguageToolLemmatizer,CoreNlpPosTagger,FastTextDDCMulLemmaNoPunctPOSNoFunctionwordsWithCategoriesTextImagerService,TagMeLocalAnnotator";
         File tFile = new File("tfile.txt");
         FileUtils.copyURLToFile(new URL("http://service.hucompute.org/urls_v2.xml"), tFile);
 
@@ -51,71 +53,59 @@ public class Test {
         JCas cas = JCasFactory.createText(sText, "de");
 
         DocumentMetaData dmd = DocumentMetaData.create(cas);
-        dmd.setDocumentTitle("Verwaldlung");
-        dmd.setDocumentId("Verwaldung-1");
+        dmd.setDocumentTitle("Die Verwandlung");
+        dmd.setDocumentId("Verwaldung-12345");
 
-        AnalysisEngineDescription aed = createEngineDescription(MediawikiWriter.class, XmiWriter.PARAM_TARGET_LOCATION,"/tmp/",XmiWriter.PARAM_OVERWRITE,true);
+        AnalysisEngineDescription aed = createEngineDescription(MediawikiWriter.class,PARAM_TARGET_LOCATION,true);
 
         Path outputXmi = Paths.get("abc.xmi");
-        
-        try {
-             // Der TextImager wird angeworfen
-            tic.process(cas.getCas(), sTools.split(","));
 
-            // Serialize & save results from the pipeline
-            /* try(OutputStream outputStream = Files.newOutputStream(outputXmi)) {
+        try {
+             
+            //Run Textimager-Client with given services (sTools)
+            //tic.process(cas.getCas(), sTools.split(","));
+
+           // Serialize & save results from the pipeline
+           /*try(OutputStream outputStream = Files.newOutputStream(outputXmi)) {
                 XMLSerializer xmlSerializer = new XMLSerializer(outputStream, true);
                 xmlSerializer.setOutputProperty(OutputKeys.VERSION, "1.0");
                 xmlSerializer.setOutputProperty(OutputKeys.ENCODING, StandardCharsets.UTF_8.toString());
                 XmiCasSerializer xmiCasSerializer = new XmiCasSerializer(null);
                 xmiCasSerializer.serialize(cas.getCas(), xmlSerializer.getContentHandler());
-            }  */
+            }*/
 
             // Read-in serialized CAS from File
             try(InputStream inputStream = Files.newInputStream(outputXmi)) {
                 XmiCasDeserializer.deserialize(inputStream, cas.getCas());
             }
-            HashSet<String> locs = extractLocations(cas);
 
-            String res = buildMapsString(locs);
-            System.out.println(res);
-//            MediawikiWriter
-            SimplePipeline.runPipeline(cas, aed);
+            /* List<MorphologicalFeatures> mList = new ArrayList<>(0);
+
+            //Get all morphological features
+            JCasUtil.select(cas, MorphologicalFeatures.class).forEach(mf->{
+                mList.add(mf);
+            });
+    
+            //Remove all morphological feature annotations in CAS
+            mList.forEach(ml->{
+                ml.removeFromIndexes();
+            }); */
+    
+            AggregateBuilder builder = new AggregateBuilder();
+    
+            AnalysisEngineDescription writer = createEngineDescription(MediawikiWriter.class, XmiWriter.PARAM_TARGET_LOCATION,"/tmp/",XmiWriter.PARAM_OVERWRITE,true);
+            builder.add(createEngineDescription(MateMorphTagger.class));
+            builder.add(writer);
+    
+            //SimplePipeline.runPipeline(cas, builder.createAggregate());
+            TTLabSimplePipeline.runPipeline(cas, builder.createAggregate());
 
 
         } catch (UIMAException e) {
             e.printStackTrace();
         } catch (Exception e) {
             e.printStackTrace();
-        }
 
     }
-
-    public static HashSet<String> extractLocations(JCas jCas){
-        HashSet<String> loc = new HashSet<String>();
-        
-        for (NamedEntity ne : JCasUtil.select(jCas, NamedEntity.class)) {
-            if (ne.getValue().equals("LOC")) {
-                String text = ne.getCoveredText();
-                loc.add(text);
-            }
-        }
-return loc;
-    }
-
-    public static String buildMapsString(HashSet<String> locs){
-        StringBuilder str = new StringBuilder();
-        if(locs.isEmpty() == true){
-            return null;
-        }else{
-            str.append("{{#display_map:");
-            Iterator<String> it = locs.iterator();
-            while(it.hasNext()) {
-                str.append(it.next());
-                str.append(";");
-            }
-            str.append("}}");
-            return str.toString();
-        }
 }
 }
